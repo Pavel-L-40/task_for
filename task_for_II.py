defbebb35c4d6d4c4ee2adda2d7a3cf770ded169
PAvel <95528324+Pavel-L-40@users.noreply.github.com>
2024-09-05 08:44:53 +0300

    task_for_II.py
    решил задание через срезы

--- task_for_II.py
+++ task_for_II.py
@@ -1,16 +1,15 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-for i in range(numbers[1], len(numbers)+1):
+for i in numbers[1:]:
     flag = True
-    for j in range(2,i):
-        if i/j in numbers:
-            flag = False
+    for j in numbers[1:]:
+        if i/j in numbers[1:]:
+            flag = not True
             break
-
-    if (flag == False):
-        not_primes.append(i)
-    elif (flag == True):
+    if (flag == True):
         primes.append(i)
+    elif (flag == False):
+        not_primes.append(i)
 print(f'Primes: {primes}')
 print(f'Not primes: {not_primes}')
